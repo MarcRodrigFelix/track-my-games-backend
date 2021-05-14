d08832e628593f3be703a4b8b3979dace2850a64
marcos <m.rodrig.felix@gmail.com>
2021-05-14 16:19:31 -0700

    edit create action in User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,35 @@
 class UsersController < ApplicationController
 
-    def new
-        @user = User.new
-    end
+  def new
+    @user = User.new
+  end
 
-    def show
-        @user = User.find(params[:id])
-        if @user
-            render json: {
-                user: @user
-            }
-        else
-            render json: {
-                status: 500,
-                errors: ['sorry, user not found']
-            }
-        end
+  def show
+    @user = User.find(params[:id])
+    if @user
+      render json: {
+        user: @user
+      }
+    else
+      render json: {
+        status: 500,
+        errors: ['Oops, user not found. Try again.']
+      }
     end
+  end
 
-    def create
-        @user = User.create(user_params)
-        if @user.valid?
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
-        else
-            render :new
-        end
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      session[:user_id] = @user.id
+      render json: {
+        user: @user
+      }
+    else
+      render json: {
+        error: @user.errors.full_messages
+      }
     end
+  end
 
 end
