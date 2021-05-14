0b1f236f0895046db1e5ea5baa1d47838036190c
marcos <m.rodrig.felix@gmail.com>
2021-05-14 15:05:38 -0700

    add validations to User for email, password, username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
     has_many :games
 
-    validates :username, presence: true
+    validates :username, :email, :password, presence: true
     validates :username, uniqueness: true
     validates :username, length: { minimum: 6 }
+    validates :email, uniqueness: true
+    validates :password, length: { in: 6..20 }
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
 end
