0b5bf6086e68b4e259c9cd95cd158423890290cc
marcos <m.rodrig.felix@gmail.com>
2021-05-14 16:15:28 -0700

    add show action for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,28 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def show
+        @user = User.find(params[:id])
+        if @user
+            render json: {
+                user: @user
+            }
+        else
+            render json: {
+                status: 500,
+                errors: ['sorry, user not found']
+            }
+        end
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
+
 end
