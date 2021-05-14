3cf1c243d7da5fa1a893fc451a0c5a7797fe01fc
marcos <m.rodrig.felix@gmail.com>
2021-05-14 14:59:56 -0700

    add username validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
     has_many :games
+
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :username, length: { minimum: 6 }
+
 end
