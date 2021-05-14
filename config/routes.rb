ff3cfac5b4ab55c6f9c88b1527e1c28d4c01d914
marcos <m.rodrig.felix@gmail.com>
2021-05-14 16:55:32 -0700

    add privat user params to User

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'session/new'
   get 'session/create'
+  
   resources :users 
   resources :games
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
