b882610739e02536d291e78bc3155d695e01889d
marcos <m.rodrig.felix@gmail.com>
2021-05-14 15:47:58 -0700

    remove password from users table

---
+++ db/migrate/20210514221127_remove_column_from_user.rb
@@ -0,0 +1,5 @@
+class RemoveColumnFromUser < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :users, :password
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_14_214139) do
+ActiveRecord::Schema.define(version: 2021_05_14_221127) do
 
   create_table "games", force: :cascade do |t|
     t.string "title"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2021_05_14_214139) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
