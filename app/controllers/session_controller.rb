4c9b4008336ee0f4333eaa32115e18ad33c34448
marcos <m.rodrig.felix@gmail.com>
2021-05-14 17:29:41 -0700

    add create action for new session

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,7 +4,16 @@ class SessionController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: session_params[:email])
     
+    if @user && @user.authenticate(session_params[:password])
+      login
+      render json: { logged_in: true, user: @user }
+    else
+      render json: { status: 401, errors: ['no such user', 'double check your login info or sign up!'] }
+    end
   end
+
+
 
 end
