592b9dc03f9ca63023d688ebe4f026fb7f52e071
marcos <m.rodrig.felix@gmail.com>
2021-05-14 17:07:24 -0700

    add login action in application controller && logged_in action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+
+    def login
+        session[:user_id] = @user.id
+    end
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
 end
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,6 +4,7 @@ class SessionController < ApplicationController
   end
 
   def create
+    
   end
-  
+
 end
