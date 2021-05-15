80bbd9cc60e49438a89a8983024bf2dc1157dec9
marcos <m.rodrig.felix@gmail.com>
2021-05-14 17:19:00 -0700

    add current user && authorized_user to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,22 @@
 class ApplicationController < ActionController::API
+  helper_method :login, :logged_in?, :current_user, :authorized_user
 
-    def login
-        session[:user_id] = @user.id
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
     end
+  end
 
-    def logged_in?
-        !!session[:user_id]
-    end
+  def login
+    session[:user_id] = user.id
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized_user
+    @user == current_user
+  end
 
 end
