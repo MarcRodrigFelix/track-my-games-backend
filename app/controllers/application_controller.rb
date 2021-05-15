b9266e28e64f7b9649010cafe172f14c1c10761c
marcos <m.rodrig.felix@gmail.com>
2021-05-15 16:10:22 -0700

    add comments to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,19 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!
 
+  # track current user
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
     end
   end
 
+  # check if user is logged in with session user id
   def login!
     session[:user_id] = @user.id
   end
 
+  # verify current user
   def logged_in?
     !!current_user
   end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
-if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_track-my-games',
-  else
-    Rails.application.config.session_store :cookie_store, key: '_track-my-games' 
-  end
+# if Rails.env === 'production' 
+#     Rails.application.config.session_store :cookie_store, key: '_track-my-games',
+#   else
+#     Rails.application.config.session_store :cookie_store, key: '_track-my-games' 
+#   end
