ac08767f89624c3527f640a36a5b9b72851974d8
marcos <m.rodrig.felix@gmail.com>
2021-05-14 17:42:31 -0700

    add private sessions params to sessions

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -27,4 +27,11 @@ class SessionController < ApplicationController
     render json: { status: 200, logged_out: true }
   end
 
+  
+  private
+
+  def session_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
