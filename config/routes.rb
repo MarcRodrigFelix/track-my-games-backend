4d8954cf7e66223fbef8a0f19a1af843708a01a0
marcos <m.rodrig.felix@gmail.com>
2021-05-14 17:46:00 -0700

    add post, delete and get path to session actions create, destroy, is logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  
-  get 'session/create'
 
-  resources :users 
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in'
+
+  resources :users, only: [ :create, :show, :index ]
   resources :games
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
