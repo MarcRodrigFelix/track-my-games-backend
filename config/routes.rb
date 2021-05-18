c2136f5b540d60f9c14f463d2711ff9f2aa25303
marcos <m.rodrig.felix@gmail.com>
2021-05-17 19:04:12 -0700

    add autologin route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  post '/login', to: 'sessions#create'
+  post '/login', to: 'sessions#login'
+  post '/autologin', to: 'sessions#login'
   delete '/logout', to: 'sessions#destroy'
   get '/logged_in', to: 'sessions#is_logged_in?'
 
   resources :users, only: [ :create, :show, :index ]
   resources :games, only: [ :create, :show, :index ]
-  
+
 end
